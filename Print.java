413db13c405eb6b6451fce243f9b725778d6e5ab
PoornimaNagendran <PoornimaNagendran@users.noreply.github.com>
2017-07-31 12:58:47 +0530

    Printing

---
+++ Print.java
@@ -0,0 +1,13 @@
+package basics;
+
+public class Print {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+if(System.out.printf("Printing without semicolon")==null)
+{
+	
+}
+	}
+
+}
